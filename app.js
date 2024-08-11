8689f5c55be8dc1ea1e18ffc692890cf9fe67a30
Kaleidoscopee <romanj1997@gmail.com>
2024-08-10 22:32:28 -0400

    Vite Downloaded (File Hierarchy Issue)

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-/*
-fetch('https://api.open-meteo.com/v1/forecast?latitude=40.758896&longitude=-73.98513&hourly=temperature_2m&temperature_unit=fahrenheit')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Data received:', data);
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
-  */
-
-/*  fetch('https://api.open-meteo.com/v1/forecast?latitude=40.758896&longitude=-73.98513&hourly=temperature_2m&temperature_unit=fahrenheit')
-.then(data => {
-return data.json();
-})
-.then(post => {
-console.log(post.title);
-}); */
-
 //WORKING
 fetch('')
   .then(response => response.json())
