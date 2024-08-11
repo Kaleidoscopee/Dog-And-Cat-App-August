60922b2cff96acf0e8182e8577b9a9257f57a535
Kaleidoscopee <romanj1997@gmail.com>
2024-08-11 04:18:04 -0400

    Fixed env issues - i think

--- Dog-And-Cat-App/src/App.jsx
+++ Dog-And-Cat-App/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //Working API structure
+  fetch(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${dogAPI}`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log("There has been an error: " + error));
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <h1>The Dog and Cat App</h1>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Location</li>
+        </ul>
+    </nav>
+    <h2>Where would you like to check the weather?</h2>
+    <h3>Search</h3>
+    <footer>
+        <h6>copyright blah blah blah 2024</h6>
+    </footer>
     </>
   )
 }
